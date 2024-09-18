b6e8d6926cc870e694449be1f999b8582f68f984
nogebeat <146479712+nogebeat@users.noreply.github.com>
2024-09-18 09:14:00 +0100

    tsdytcg

---
+++ App_container/App_container/src/script_ubuntu.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# #!/bin/bash
+
+
+# sudo apt install docker.io docker-compose
+
+# systemctl enable --now docker
+
+# systemctl status docker
+
+# groupadd docker
+
+# sudo usermod -aG docker $USER
+
+# docker --version
+
+
+# sudo cp ./src/coding-style /usr/bin
+
+# coding-style . .
+
+# ./src/epitech-emacs/INSTALL.sh local
+
+cd src/epitech-emacs/
+
+clear
+
+ls
+
+./INSTALL.sh local
+
+
+tput setaf 1
+	echo "__**/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/CODING STYLE BY NOGE\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\**\*\*\*\*\**__"
+	echo " _           _"
+        echo "| |\\        | | NOGE"
+        echo "| |\\\       | |"
+        echo "| | \\\      | | EPITECH BENIN"
+        echo "| |  \\\     | |"
+        echo "| |   \\\    | | NOGE"
+        echo "| |    \\\   | |"
+        echo "| |     \\\  | | EPITECH DIGITAL SCHOOL"
+        echo "| |      \\\ | |"
+        echo "|_|       \\\|_| NOGE"
+	tput sgr0
+
+echo "alias emacs='emacs -nw'" >> ~./bashrc
+
+# ./INSTALL.sh system
