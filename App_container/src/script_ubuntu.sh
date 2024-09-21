7d7c51da4dc799ec5986821e1bc7c0f8f9a3dd9f
nogebeat <146479712+nogebeat@users.noreply.github.com>
2024-09-21 19:18:02 +0100

    Correction globale du script et Compression apres test

--- App_container/src/script_ubuntu.sh
+++ App_container/src/script_ubuntu.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 
-# #!/bin/bash
 
-sudo apt-get update -y
+sudo apt-get install -y gcc make cmake perl curl wget build-essential ocaml libncurses5-dev libncursesw5-dev git libx11-dev libxmu-dev libxi-dev libgl-dev libopenal-dev libxrandr-dev libudev-dev libglew-dev libjpeg-dev libalut-dev libcsfml-dev libqt5widgets5 libqt5websockets5 libqt5webengine5 libqt5opengl5
 
-sudo apt-get install -y gcc emacs* make \
-                        cmake \
-                        perl \
-                        gcc \
-                        curl \
-                        wget \
-                        build-essential \
-                        ocaml \
-                        libncurses* \
-                        git \
-                        libx11-dev \
-                        libxmu-dev \
-                        libxi-dev \
-                        libgl-dev \
-                        libopenal-dev \
-                        libxrandr-dev \
-                        libudev-dev \
-                        libglew-dev \
-                        libjpeg-dev \
-                        libalut-dev \
-                        libcsfml* \
-                        libqt5*
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y emacs 
+
+
+sudo apt --fix-broken install -y 
+
+sudo snap install emacs -y
 
 unzip ./src/discord.zip
 
@@ -35,51 +18,44 @@ sudo dpkg -i ./src/algobox.deb
 
 sudo apt install -y docker.io docker-compose
 
-  snap install teams-for-linux
+snap install teams-for-linux
 
-systemctl enable --now docker
+sudo systemctl enable --now docker
 
-# systemctl status docker
-
-groupadd docker
-
+sudo groupadd docker
 sudo usermod -aG docker $USER
 
 docker --version
-
 
 sudo cp ./src/coding-style /usr/bin
 
 coding-style . .
 
-./src/epitech-emacs/INSTALL.sh local
+sudo cp -r ./src/epitech-emacs /usr/local/bin/
+sudo chmod +x /usr/local/bin/epitech-emacs/*
 
-cd src/epitech-emacs/
+/usr/local/bin/epitech-emacs/INSTALL.sh local
 
-clear
+cd ./src/epitech-emacs/
 
 ls
 
-./INSTALL.sh local
+sudo ./INSTALL.sh system
 
-echo "alias emacs='emacs -nw'" >> ~/.bashrc
+echo "alias emacs='emacs -nw'" | sudo tee -a /etc/bash.bashrc
 
 source ~/.bashrc
 
 tput setaf 1
-	echo "__**/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/CODING STYLE BY NOGE\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\**\*\*\*\*\**__"
-	echo " _           _"
-        echo "| |\\        | | NOGE"
-        echo "| |\\\       | |"
-        echo "| | \\\      | | EPITECH BENIN"
-        echo "| |  \\\     | |"
-        echo "| |   \\\    | | NOGE"
-        echo "| |    \\\   | |"
-        echo "| |     \\\  | | EPITECH DIGITAL SCHOOL"
-        echo "| |      \\\ | |"
-        echo "|_|       \\\|_| NOGE"
-	tput sgr0
-
-
-
-# ./INSTALL.sh system
+echo "__**/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/CODING STYLE BY NOGE\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\**\*\*\*\*\**__"
+echo " _           _"
+echo "| |\\        | | NOGE"
+echo "| |\\\       | |"
+echo "| | \\\      | | EPITECH BENIN"
+echo "| |  \\\     | |"
+echo "| |   \\\    | | NOGE"
+echo "| |    \\\   | |"
+echo "| |     \\\  | | EPITECH DIGITAL SCHOOL"
+echo "| |      \\\ | |"
+echo "|_|       \\\|_| NOGE"
+tput sgr0
