1bac9b5a86676b7fd4fb41523dbc268fa2b0a042
Noge <146479712+nogebeat@users.noreply.github.com>
2024-09-29 21:34:54 +0100

    Create header-epitech.sh

---
+++ header-epitech.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+clear
+
+cd /tmp
+
+git clone https://github.com/nogebeat/deb-ubuntu-epitech.git
+
+git clone https://github.com/Epitech/epitech-emacs.git
+
+ cd epitech-emacs
+
+./INSTALL.sh local
+
+tput setaf 1
+echo "__**/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/CODING STYLE BY NOGE\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\**\*\*\*\*\**__"
+echo " _           _"
+echo "| |\\        | | NOGE"
+echo "| |\\\       | |"
+echo "| | \\\      | | EPITECH BENIN"
+echo "| |  \\\     | |"
+echo "| |   \\\    | | NOGE"
+echo "| |    \\\   | |"
+echo "| |     \\\  | | EPITECH DIGITAL SCHOOL"
+echo "| |      \\\ | |"
+echo "|_|       \\\|_| NOGE"
+tput sgr0
