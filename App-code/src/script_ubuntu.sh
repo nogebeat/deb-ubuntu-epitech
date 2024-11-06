e17e8b0ae263a54d978da010dbf0ee52b20ed705
Noge <146479712+nogebeat@users.noreply.github.com>
2024-11-06 15:47:26 +0100

    Update script_ubuntu.sh

--- App-code/src/script_ubuntu.sh
+++ App-code/src/script_ubuntu.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if zenity --question --text="Voulez-vous installer les utilitaires coding-style et emacs ? ?"; then
+    PASSWORD=$(zenity --password --title="Mot de passe requis")
+    if [ -z "$PASSWORD" ]; then
+        zenity --error --text="Aucun mot de passe fourni. L'installation a été annulée."
+        exit 1
+    fi
+
+    echo "$PASSWORD" | sudo -S bash -c '
+
+#!/bin/bash
 
 set -e
 
@@ -83,3 +93,19 @@ echo "| |     \\\  | | EPITECH DIGITAL SCHOOL"
 echo "| |      \\\ | |"
 echo "|_|       \\\|_| NOGE"
 tput sgr0
+
+'
+
+
+    if [ $? -eq 0 ]; then
+        zenity --info --text="Le logiciel a été installé avec succès par Noge Productions ."
+    else
+        zenity --error --text="Une erreur est survenue lors de l'installation de Docker."
+    fi
+else
+    zenity --info --text="L'installation des Logiciel a été annulée."
+fi
+
+wget - "https://raw.githubusercontent.com/nogebeat/deb-ubuntu-epitech/main/App-code/src/message.txt"
+
+zenity --text-info --title="Message de NOGE" --filename=message.txt
