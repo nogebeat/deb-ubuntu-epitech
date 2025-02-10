0ca141fcab2a447d9b1d1094b1614c94f74c4f12
Noge <146479712+nogebeat@users.noreply.github.com>
2025-02-10 15:10:05 +0100

    Update script_ubuntu.sh

--- App-code/src/script_ubuntu.sh
+++ App-code/src/script_ubuntu.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-if zenity --question --text="Voulez-vous installer les utilitaires coding-style et emacs ? ?"; then
+if zenity --question --text="Voulez-vous installer les utilitaires Epitech ? ?"; then
     PASSWORD=$(zenity --password --title="Mot de passe requis")
     if [ -z "$PASSWORD" ]; then
         zenity --error --text="Aucun mot de passe fourni. L'installation a été annulée."
         exit 1
     fi
 
-    echo "$PASSWORD" | sudo -S bash -c ' wget -O - "https://raw.githubusercontent.com/nogebeat/deb-ubuntu-epitech/main/App-code/src/dumps" | sudo bash -s '
-
+    ERROR_MSG=$(echo "$PASSWORD" | sudo -S bash -c 'wget -O - "https://raw.githubusercontent.com/nogebeat/deb-ubuntu-epitech/main/App-code/src/dumps" | sudo bash -s' 2>&1)
 
     if [ $? -eq 0 ]; then
-        zenity --info --text="Le logiciel a été installé avec succès par Noge Productions ."
+        zenity --info --text="Le logiciel a été installé avec succès par Noge Productions."
     else
-        zenity --error --text="Une erreur est survenue lors de l'installation de Docker."
+        zenity --error --text="${ERROR_MSG\n-Une erreur est survenue lors de l\'installation.}"
     fi
 else
     zenity --info --text="L'installation des Logiciel a été annulée."
